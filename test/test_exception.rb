436a44ce931ff0364a2e99f40bd08a77950842f5
quix <quixoticsycophant@gmail.com>
2009-04-14 01:54:51 -0400

    add test for abort_on_exception

--- test/test_exception.rb
+++ test/test_exception.rb
@@ -4,45 +4,53 @@ class TestException < Test::Unit::TestCase
   def test_exception
     test_error = Class.new StandardError
     [true, false].each { |define_all|
-      error = (
-        begin
-          CompTree.build { |driver|
-            driver.define(:area, :width, :height, :offset) {
-              |width, height, offset|
-              width*height - offset
-            }
-            
-            driver.define(:width, :border) { |border|
-              2 + border
-            }
-
-            driver.define(:height, :border) { |border|
-              3 + border
-            }
-            
-            if define_all
-              driver.define(:border) {
-                raise test_error
+      [true, false].each { |abort_on_exception|
+        error = (
+          begin
+            CompTree.build { |driver|
+              driver.define(:area, :width, :height, :offset) {
+                |width, height, offset|
+                width*height - offset
               }
-            end
-            
-            driver.define(:offset) {
-              7
+              
+              driver.define(:width, :border) { |border|
+                2 + border
+              }
+  
+              driver.define(:height, :border) { |border|
+                3 + border
+              }
+              
+              if define_all
+                driver.define(:border) {
+                  raise test_error
+                }
+              end
+              
+              driver.define(:offset) {
+                7
+              }
+        
+              begin
+                previous = Thread.abort_on_exception
+                Thread.abort_on_exception = abort_on_exception
+                driver.compute(:area, 99) 
+              ensure
+                Thread.abort_on_exception = previous
+              end
             }
-      
-            driver.compute(:area, 99) 
-          }
-          nil
-        rescue => e
-          e
+            nil
+          rescue => e
+            e
+          end
+        )
+  
+        if define_all
+          assert_block { error.is_a? test_error }
+        else
+          assert_block { error.is_a? CompTree::NoFunctionError }
         end
-      )
-
-      if define_all
-        assert_block { error.is_a? test_error }
-      else
-        assert_block { error.is_a? CompTree::NoFunctionError }
-      end
+      }
     }
-  end
-end 
+  end 
+end
