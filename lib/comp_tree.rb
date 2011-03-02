fb3271dc53b88d043198fafaacee36c900bf1064
quix <quixoticsycophant@gmail.com>
2011-03-02 13:04:30 -0500

    add version.rb; remove comp_tree/comp_tree.rb.

--- lib/comp_tree.rb
+++ lib/comp_tree.rb
@@ -27,4 +27,54 @@ require 'comp_tree/queue/queue'
 require 'comp_tree/node'
 require 'comp_tree/algorithm'
 require 'comp_tree/driver'
-require 'comp_tree/comp_tree'
+
+#
+# CompTree -- Parallel Computation Tree.
+#
+# See README.rdoc.
+#
+module CompTree
+  #
+  # :call-seq:
+  #   build { |driver| ... }
+  #
+  # Build a new computation tree. A Driver instance is passed to the
+  # given block.
+  #
+  # Example:
+  #   CompTree.build do |driver|
+  # 
+  #     # Define a function named 'area' taking these two arguments.
+  #     driver.define(:area, :width, :height) { |width, height|
+  #       width*height
+  #     }
+  #     
+  #     # Define a function 'width' which takes a 'border' argument.
+  #     driver.define(:width, :border) { |border|
+  #       7 + border
+  #     }
+  #     
+  #     # Ditto for 'height'.
+  #     driver.define(:height, :border) { |border|
+  #       5 + border
+  #     }
+  # 
+  #     #    
+  #     # Define a constant function 'border'.
+  #     driver.define(:border) {
+  #       2
+  #     }
+  #     
+  #     # Compute the area using four parallel threads.
+  #     puts driver.compute(:area, 4)
+  #     # => 63
+  # 
+  #     # We've done this computation.
+  #     puts((7 + 2)*(5 + 2))
+  #     # => 63
+  #   end
+  #
+  def self.build(opts = {})
+    yield Driver.new(opts)
+  end
+end
